f6b34f7f911303f722f8669bf85d8ac2aedcc328
Fabian Dennler <fd@no-reply.fabforge.ch>
2015-11-06 11:17:46 +0100

    CRLF, id as input param
    removed CRLF.
    Added possibily to pass a quote id to ignore

--- includes/session.inc.php
+++ includes/session.inc.php
@@ -2,6 +2,11 @@
 session_start();
 $_SESSION['time'] = time();
 $_SESSION['quote'] = '';
+// $id = filter_input(INPUT_GET, 'id');
+$id = isset($_GET['id']) ? (int) $_GET['id'] : null;
+if ($id) {
+  $_SESSION['index'] = sprintf('%d', $id);
+}
 if (!isset($_SESSION['index'])) {
   $_SESSION['index'] = 0; // store last quote id in the session 
 }
