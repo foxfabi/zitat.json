04c638dd953057a715a715c75434beafeb3a4a66
Fabian Dennler <fd@no-reply.fabforge.ch>
2015-11-06 11:16:18 +0100

    changed output format
    changed output format while ". -" was messy

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,6 +1,6 @@
 CREATE FUNCTION concatQuoteAuthor (q VARCHAR(255), a VARCHAR(255)) 
 RETURNS VARCHAR(255)
-RETURN CONCAT(q, ' - ',a,'.');
+RETURN CONCAT(q, ' (', a,')');
 
 
 CREATE FUNCTION quoteLength (s VARCHAR(255))
