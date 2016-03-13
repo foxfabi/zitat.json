707b35973e8b4bd684b0c9eb235e37f3fcc13b39
Fabian Dennler <fd@no-reply.fabforge.ch>
2016-03-13 16:59:59 +0100

    Wrong ID output from join

--- sql/views.sql
+++ sql/views.sql
@@ -1,5 +1,5 @@
 CREATE VIEW missingSequenceNumbers AS 
-  SELECT l.ID AS start 
+  SELECT l.ID + 1 AS start 
   FROM citation AS l 
   LEFT OUTER JOIN citation AS r ON l.ID + 1 = r.ID 
   WHERE r.ID is null 
